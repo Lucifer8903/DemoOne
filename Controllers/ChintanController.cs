566ebcba02202910cbbbf272fd3b2c666d4677d6
chintan dankhara <chintandankhara9@gmail.com>
2026-04-02 21:54:43 +0530

    chintan added

---
+++ Controllers/ChintanController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoOne.Controllers
+{
+    [Route("[controller]")]
+    public class ChintanController : Controller
+    {
+        private readonly ILogger<ChintanController> _logger;
+
+        public ChintanController(ILogger<ChintanController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
